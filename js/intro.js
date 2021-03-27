b533c48c487b8ff574c059f709a4d8b9f1094e5a
lybell <32293736+lybell-art@users.noreply.github.com>
2021-03-27 21:56:29 +0900

    Create intro.js

---
+++ js/intro.js
@@ -0,0 +1,26 @@
+function vmin(p)
+{
+  return Math.min(window.innerWidth,window.innerHeight) * p / 100;
+}
+
+let Showcase_Button=function(d)
+{
+	d.radius=0;
+	d.buho=1;
+	d.setup=function()
+	{
+		d.createCanvas(vmin(20), vmin(20));
+		d.background(255);
+	};
+	d.draw=function()
+	{
+		d.ellipse(d.mouseX,d.mouseY,d.radius,d.radius);
+		if(d.radius>60||d.radius<0) d.buho=-d.buho;
+		d.radius+=d.buho;
+	};
+	d.windowResized=function()
+	{
+		d.resizeCanvas(vmin(20),vmin(20));
+	}
+};
+new p5(sketch,"showcase_button");
