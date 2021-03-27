c9041e872d4748b4d5be455fafe719e679159ad7
lybell <32293736+lybell-art@users.noreply.github.com>
2021-03-27 23:05:59 +0900

    Update intro.js

--- js/intro.js
+++ js/intro.js
@@ -2,26 +2,44 @@ function vmin(p)
 {
   return Math.min(window.innerWidth,window.innerHeight) * p / 100;
 }
+function between(input, a, b)
+{
+  return a<=input && input<=b;
+}
 
 let Showcase_Button=function(d)
 {
-	d.radius=0;
-	d.buho=1;
+	d.teapot=null;
+	d.rot=0.0;
+	d.preload=function() {
+		d.teapot = loadModel('assets/teapot.obj', true);
+	}
 	d.setup=function()
 	{
-		d.createCanvas(vmin(20), vmin(20));
-		d.background(255);
+		d.createCanvas(vmin(20), vmin(20), WEBGL);
+		d.noStroke();
+		d.rot=0.0;
 	};
 	d.draw=function()
 	{
-		d.ellipse(d.mouseX,d.mouseY,d.radius,d.radius);
-		if(d.radius>60||d.radius<0) d.buho=-d.buho;
-		d.radius+=d.buho;
+		let isMouseOn=this.mouseOn();
+		d.clear();
+		d.lights();
+		d.fill(isMouseOn ? "#FF8C9A" : "#FFF4E7");
+		d.rotateX(HALF_PI);
+		d.rotateZ(d.rot);
+		d.scale(6 * (isMouseOn?1.2:1));
+		d.shape(d.teapot);
+		d.rot+=0.2;
 	};
 	d.windowResized=function()
 	{
-		console.log("why");
 		d.resizeCanvas(vmin(20),vmin(20), false);
+	}
+	
+	d.isMouseOn=function()
+	{
+		return (between(mouseX,0,d.width) && between(mouseY,0,d.height));
 	}
 };
 new p5(Showcase_Button, 'showcase_sketch');
