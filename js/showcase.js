564f36deabc27efbc2dc536c63c4697d4bcef82c
lybell <32293736+lybell-art@users.noreply.github.com>
2021-03-29 00:25:13 +0900

    Update showcase.js

--- js/showcase.js
+++ js/showcase.js
@@ -152,7 +152,7 @@ function setup()
 	noStroke();
 	slider1=createSlider(-100, 100, -50);
 	slider2=createSlider(-100, 100, 58);
-	slider3=createSlider(0, 200, 100);
+	slider3=createSlider(0, 500, 100);
 	slider4=createSlider(-200, 200, 0);
 	slider1.position(10, 10);
 	slider2.position(10, 40);
@@ -188,9 +188,9 @@ function draw()
 	
 	//drawing trees
 	push();
-	scale(0.7);
+	scale(0.4);
 	rotateX(PI);
-	translate(180,5,-6*slider3.value());
+	translate(300,5,-6*slider3.value());
 	fill(65, 166, 159);
 	for(var i=0; i<13; i++)
 	{
