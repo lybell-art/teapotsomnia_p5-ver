e1bf943965f09e46812284461771e7aefd358602
lybell <32293736+lybell-art@users.noreply.github.com>
2021-03-29 04:42:56 +0900

    Update showcase.js

--- js/showcase.js
+++ js/showcase.js
@@ -234,17 +234,19 @@ function draw()
 	
 	clear();
 	//setting camera and light
-	camera(0,0, (height/2.0) / tan(PI*30.0 / 180.0),0,-100,0,0,1,0);
+//	camera(0,0, (height/2.0) / tan(PI*30.0 / 180.0),0,-100,0,0,1,0);
 	lights();
 	ambientLight(34,5,15);
 	directionalLight(135,135,135, -1, 1, -1);
 	//setting position
 	
 	cameraMove();
-	translate(transX,transY,0);
-	rotateX(rotX);
-	rotateY(-rotY);
-	scale(1+0.1*scaleFactor);
+//	translate(transX,transY,0);
+//	rotateX(rotX);
+//	rotateY(-rotY);
+//	scale(1+0.1*scaleFactor);
+	
+	camera(transX,transY,(height/2.0) / tan(PI*30.0 / 180.0) * scaleFactor, rotX, rotY, 0);
 	
 	//drawing terrain
 	push();
