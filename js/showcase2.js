926e9a5f51297f94e97ab3facb03485ededadb1a
lybell <32293736+lybell-art@users.noreply.github.com>
2021-03-29 17:00:42 +0900

    Update showcase2.js

--- js/showcase2.js
+++ js/showcase2.js
@@ -1,10 +1,9 @@
-let today = new Date();   
-let hours = today.getHours();
-
 const bodyTag=document.getElementsByTagName("body")[0];
-if(between(hours,10,14)) bodyTag.classList.add('noon');
-else if(between(hours,0,6) || between(hours,21,24)) bodyTag.classList.add('midnight');
-
+switch(timeDetect())
+{
+	case _NOON:bodyTag.classList.add('noon'); break;
+	case _NIGHT:bodyTag.classList.add('midnight'); break;
+}
 
 var video = document.querySelector("#webcam");
  
